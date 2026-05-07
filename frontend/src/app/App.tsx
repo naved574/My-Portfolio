e17d3ca2091edb994a3d5b4bf35d2ffe997f9f53
naved574 <navedansari2543@gmail.com>
2026-05-07 22:34:20 +0530

    fix bug

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import AppRouter from "./AppRouter.tsx";
+import AppRouter from "./AppRouter";
 
 
 const queryClient = new QueryClient();
