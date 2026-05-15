36b91b3316555f544a5fb46b29bd45d0a39f5bfd
naved574 <navedansari2543@gmail.com>
2026-05-15 16:45:05 +0530

    fix run build error

--- frontend/src/pages/Index.ts
+++ frontend/src/pages/Index.ts
@@ -1,5 +1,5 @@
-export { default as Home } from "./home/Home";
-export { default as About } from "./about/About";
-export { default as Projects } from "./projects/Projects";
-export { default as Contact } from "./contact/Contact";
+export { default as Home } from "./home/Home.tsx";
+export { default as About } from "./about/About.tsx";
+export { default as Projects } from "./projects/Projects.tsx";
+export { default as Contact } from "./contact/Contact.tsx";
 export { default as NotFound } from "./NotFound";
